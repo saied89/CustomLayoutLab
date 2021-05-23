a9f39ad13a69018617e71c7a71ae77558bc5a4eb
Saeed Momenzadeh <saied.momen@balad.ir>
2021-05-23 20:32:31 +0430

    add bottomsheet code and sample

--- app/src/main/java/com/example/customlayoutlab/MainActivity.kt
+++ app/src/main/java/com/example/customlayoutlab/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.customlayoutlab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.customlayoutlab.ui.theme.CustomLayoutLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +26,7 @@ class MainActivity : ComponentActivity() {
             CustomLayoutLabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    BottomSheetScaffoldSample()
                 }
             }
         }
@@ -26,7 +35,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text( text = "Hello $name!", modifier = Modifier.firstBaselineToTop(48.dp))
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = layout { measurable, constraints ->
+    // Measure the composable
+    val placeable = measurable.measure(constraints)
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    // Height of the composable with padding - first baseline
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+        // Where the composable gets placed
+        placeable.placeRelative(0, placeableY)
+    }
 }
 
 @Preview(showBackground = true)
